cd2c7985981f7bd45934e2017741e48c95254df3
Diogo Rodrigo Schimmelpfennig <diogo_ti@hotmail.com.br>
2020-12-07 00:11:07 -0400

    Adição do projeto

---
+++ TrabalhandoDataTable/DataTableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace TrabalhandoDataTable
+{
+    public static class DataTableExtensions
+    {
+        public static DataTable CreateNewDataTable(DataTable dtOrigem)
+        {
+            var newDt = new DataTable();
+            foreach (DataColumn dc in dtOrigem.Columns)
+                newDt.Columns.Add(dc.ColumnName, dc.DataType);
+
+            return newDt;
+        }
+
+        public static void AddRowDataTable(this DataTable dtOrigem, DataRow drToAdd)
+        {
+            var newRow = dtOrigem.NewRow();
+            foreach (DataColumn dc in dtOrigem.Rows)
+                newRow[dc.ColumnName] = drToAdd[dc.ColumnName];
+
+            dtOrigem.Rows.Add(newRow);
+        }
+    }
+}
---
+++ TrabalhandoDataTable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+
+namespace TrabalhandoDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var dtDadosPk = CriarDataTableWithPk(); 
+            Console.WriteLine($"Quantidade de Clientes {dtDadosPk.Rows.Count}");
+
+            var dtFinal = DataTableExtensions.CreateNewDataTable(dtDadosPk);
+
+            for (int i = 0; i < 10; i++)
+                dtFinal.AddRowDataTable(dtDadosPk.Rows[0]);
+
+        } 
+         
+        private static DataTable CriarDataTableWithPk()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("idCliente", typeof(int));
+            dt.Columns.Add("nomeCliente", typeof(string));
+
+            dt.PrimaryKey = new DataColumn[] { dt.Columns["idCliente"] };
+
+            for (int i = 0; i < 10; i++)
+            {
+                var row = dt.NewRow();
+                row["idCliente"] = i + 1;
+                row["nomeCliente"] = $"Cliente {i + 1}";
+
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        } 
+    }
+}
